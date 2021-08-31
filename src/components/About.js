7dcdb0a24622a1386dce59e8f3ce39e842fda0e9
ParmarNikhil <np0598029@gmail.com>
2021-08-31 11:09:59 +0530

    added automated deployment using github pages

--- src/components/About.js
+++ src/components/About.js
@@ -6,20 +6,23 @@ export default class About extends Component {
       <div className="about-container">
         
           <div className="about" id="about">
-            <h2 style={{fontSize:30}}>I'm a student</h2>
-            <p className="about-text">
-             I'm currently pursuing my masters degree in computer technology at GLS Univercity ahmedabad,
+            <h2 style={{fontSize:30}} className="abh2"><span>I'm a student</span></h2>
+            <p className="about-text" id="aboutt">
+              <span>
+             I'm currently pursuing my masters degree in computer technology at GLS University ahmedabad,
              I have knowledge of programming languages like python, javascript, Java. 
+             </span>
             </p>
             <p className="about-text">
+              <span>always ready to learn and explore new things and stay consistent to my work.</span>
+            </p>
+            
+            <p className="about-text">
+            <span>
             I have created some projects in python and javascript you can check them in projects section.
+            </span>
             </p>
-            <p className="about-text">
-              always eager to learn and explore new things and stay consistent to my work.
-            </p>
-                  
-                  
-                                   
+                                 
           </div>
          
           </div>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,20 +8,25 @@ export default class Projects extends Component {
               <div className="project">
                 <p style={{margin:0, fontSize:22}}>FoodZilla</p>
                 <p>it's an online recipe web application built in javascript(ReactJs)</p>
-                <a href="https://github.com/ParmarNikhil/FoodZilla-Web"><button>Github Link</button></a>
-                <a href="https://ParmarNikhil.github.io/FoodZilla-Web"><button>Check live</button></a>
+                <div className="buttonholder">
+                <a href="https://github.com/ParmarNikhil/FoodZilla-Web"><button className="buttonlink"><span>Github Link</span></button></a>
+                <a href="https://ParmarNikhil.github.io/FoodZilla-Web"><button className="buttoncheck"><span>Check live</span></button></a>
+                </div>
               </div>
               <div className="project">
               <p style={{margin:0, fontSize:22}}>Quizzo</p>
                 <p>it's an online quiz application built in python(Django Framework) </p>
-                <a href="https://github.com/ParmarNikhil/Quizzo"><button className="ghlink">Github Link</button></a>
-             
+                <div className="buttonholder">
+                <a href="https://github.com/ParmarNikhil/Quizzo"><button className="buttonlink"><span>Github Link</span></button></a>
+                </div>
               </div>
               <div className="project">
               <p style={{margin:0, fontSize:22}}>TodoList</p>
                 <p>it's a simple todo app built in javascript(ReactJs)</p>
-                <a href="https://github.com/ParmarNikhil/TodoApp"><button className="ghlink">Github Link</button></a>
-                <a href="https://ParmarNikhil.github.io/TodoApp"><button className="ghlink">Check live</button></a>
+                <div className="buttonholder">
+                <a href="https://github.com/ParmarNikhil/TodoApp"><button className="buttonlink"><span>Github Link</span></button></a>
+                <a href="https://ParmarNikhil.github.io/TodoApp"><button className="buttoncheck"><span>Check live</span></button></a>
+                </div>
               </div>  
           </div>
 
